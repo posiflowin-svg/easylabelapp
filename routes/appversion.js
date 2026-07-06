95d973ed7ca4a58bf9b5b451b16921c3f9ef38b5
posiflowin-svg <posiflow.in@gmail.com>
2026-07-06 12:14:45 +0530

    Fix app version update save functionality

--- routes/appversion.js
+++ routes/appversion.js
@@ -1,14 +1,14 @@
-const express = require('express')
-const r̥outer  = express.Router()
+const express = require('express');
+const router = express.Router();
 
-const AppVersionController = require('../controllers/AppVersionController')
+const AppVersionController = require('../controllers/AppVersionController');
 
-r̥outer.get('/', AppVersionController.index)
-r̥outer.post('/show', AppVersionController.show)
-r̥outer.post('/store', AppVersionController.store)
-r̥outer.post('/update', AppVersionController.update)
-r̥outer.post('/delete', AppVersionController.destroy)
-r̥outer.get('/latest', AppVersionController.getLatestVersion)
-r̥outer.post('/update-latest', AppVersionController.updateLatestVersion)
+router.get('/', AppVersionController.index);
+router.post('/show', AppVersionController.show);
+router.post('/store', AppVersionController.store);
+router.post('/update', AppVersionController.update);
+router.post('/delete', AppVersionController.destroy);
+router.get('/latest', AppVersionController.getLatestVersion);
+router.post('/update-latest', AppVersionController.updateLatestVersion);
 
-module.exports = r̥outer
+module.exports = router;
