190b003f904c6dc7d40c18c16220f93fa1ad2043
posiflowin-svg <posiflow.in@gmail.com>
2026-09-16 14:36:10 +0530

    Add Last Purchase Date and Posiflow CRM order history integration

--- routes/usageAnalytics.js
+++ routes/usageAnalytics.js
@@ -7,4 +7,5 @@ router.get('/customers', c.customers);
 router.get('/recent', c.recent);
 router.get('/models', c.models);
 router.get('/customer-activity', c.customerActivity);
+router.get('/purchase-history', c.purchaseHistory);
 module.exports = router;
