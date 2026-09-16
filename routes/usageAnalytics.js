705b5b85f3457ae60e5ed92c45049d4c5b4f430f
posiflowin-svg <posiflow.in@gmail.com>
2026-09-16 14:56:48 +0530

    Fix usage analytics purchase history route

--- routes/usageAnalytics.js
+++ routes/usageAnalytics.js
@@ -7,5 +7,14 @@ router.get('/customers', c.customers);
 router.get('/recent', c.recent);
 router.get('/models', c.models);
 router.get('/customer-activity', c.customerActivity);
-router.get('/purchase-history', c.purchaseHistory);
+router.get('/purchase-history', async (req, res, next) => {
+  try {
+    if (typeof c.purchaseHistory !== 'function') {
+      return res.status(503).json({ success: false, message: 'Purchase history service is unavailable' });
+    }
+    return await c.purchaseHistory(req, res, next);
+  } catch (err) {
+    return next(err);
+  }
+});
 module.exports = router;
